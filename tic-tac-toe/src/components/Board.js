f863e9c45582ff746a5f105e1b23464dfc778909
harin9 <83578728+harin9@users.noreply.github.com>
2023-09-04 13:59:19 +0900

    feat: go back to prev state!

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -1,32 +1,38 @@
 import "./Board.css";
-import React, { Component } from "react";
+import React from "react";
 import Square from "./Square";
 
-export default class Board extends Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
+const Board = ({ squares, onClick }) => {
+  const renderSquare = (i) => {
     return (
-      <div>
-        <div className="status">Next Player: X, O</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <Square
+        value={squares[i]}
+        onClick={() => {
+          onClick(i);
+        }}
+      />
     );
-  }
-}
+  };
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -1,8 +1,17 @@
 import "./Square.css";
 import React from "react";
 
-export default class Square extends React.Component {
-  render() {
-    return <button className="square"></button>;
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        onClick();
+      }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
